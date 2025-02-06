953b84c0a8e5396c51318e7198cb6109d457a2d5
leilingzero <66689331+leilingzero@users.noreply.github.com>
2025-02-06 17:59:24 +0800

    Update 剧圈圈.js

--- XYQ/js/剧圈圈.js
+++ XYQ/js/剧圈圈.js
@@ -4,7 +4,8 @@ Object.assign(muban.mxpro.二级, {
 var rule = {
     模板: 'mxpro',
     title: '剧圈圈',
-    host: 'https://www.jqqzx.cc/',
+//  host: 'https://www.jqqzx.cc/',
+    host: 'https://www.jqqzx.me/',
     url: '/vodshow/id/fyclass/page/fypage.html',
     searchUrl: '/vodsearch**/page/fypage.html',
     class_parse: '.navbar-items li:gt(2):lt(8);a&&Text;a&&href;.*/(.*?)\.html',
