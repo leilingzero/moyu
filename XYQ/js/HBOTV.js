5b987fdf995903b07a9a2185b2ed6fcab6fa8916
leilingzero <66689331+leilingzero@users.noreply.github.com>
2025-03-27 11:48:02 +0800

    Update HBOTV.js

--- XYQ/js/HBOTV.js
+++ XYQ/js/HBOTV.js
@@ -1,1 +1,64 @@
-H4sIAIioBmcC/6VX227bRhB971cskkIkHfGiS3yRyqaOWzSoc2ljO0XhGMqKHIlr82buUhcnAvoBbZHHPvSpL+0H9KEI+jlB0s/o7JK6ULZSFPUDtRyeOTM7MzuzHtGMZHkIxCUvPyJEMBFCh2gP7j85fnb69q+fz7Q6ioOEC5QGQqS8Y9vj8dgK+okYNSwviWwFybMQEfZg6oWUc5vFPkx6g2lKh2AFIgpPq5+UrCDnQDMvOCn0R4lfvNtbW2ahbq+gaF+615SCy5x5F0dK2iGOlAxYKCArIEoQAPUh4x21NUK0Ew6ZuT+EWGho6+uD3sm+4p7Jh3Kul9KMywBYXOTMLAh6vQjinISsS2u1Y5gI+RtkMOhaW7Zubd0zChfTkE7nBCLLQcpCejXtkFvnvPM8xjcQRG4co53BZQ5c6CxOc2F0F1+9JB7gZ4myIiq8QLczxYx+oClX2fvENk4bZwulc57EqPPV0ZPHdy3lgK54lrSYHQRInIVLJWYDoisBxF42TQVxXaI1NIO8lF9JqZLHwD2ago6vBd2MQMjhZvXmRvU+5bDd/hy8xC+4SrK5J/bz55YetfLdV1HafhW1Kf62DNsSMkYKPydWAZPlWrxi2IuIO/W54Hyy+qYKEx+lYLa6iVXKQn5LJY1FDOt9W679JFcFNU/ou59+//vH11gheVjUCFZsyLiwvBBrccAm3aJO1EnqWjL/YUL9Ws2ngppJxoYspmHXSplnCqylakWpOnr75/fv3/z2ARPk/5t484MyURyM+akvrGHlCDwivZ4PgrKQWOpzSXIjpFZLO3Cpt4xarTwhyggGNhpepxVBHvXJJrdLTR/rRqqu7eFfzctF0/gv6MYH0SQ1TZ7S+MOYgmcVWW6jRMqdFFiTXwAe6gpI0D5fhAkZYgh7Pdl6SNAqITL9/HooZV+Qn6T928w3ELdsaO9e//L+j1875ONzbonkSGQsHuq6QdxPy7TLvxH2fx9P0+lZtyLrJ/4UxdoFTPsJzXxXI3fI4Rff4VOr8SAZu0VVCIhS5ruNmujHNAI3hjGvRUriJymODbdo3FqX2HaYDHVJbFRMlV0arb1Y+qX69QPUl526Om/KkCxQZTTMEOKhCCR+Z+8ahnoBqLhlSSghEZ2YOFtcZx2Zp8MM/TFZzMHLMzDLRs2lVmMdXVSttnE2bvJVTFOQWjRNQ+ZRwZLYnpioaw6SLDKxXWFbxVbpX3NPjjA6H2GPkisWhtS+azlEf8jifNIl+7GfJcwnjWaXNJtOs9FsNJ4ekPs5C3376PCbhoUCx9m2HKfRJeORQfbRCfgW+odM2HdbO1Zrm+iHD44fPaxjPV0A+RK8i8QgzzBJ0s82GjsIsiQCe2/Pcqz2brtt7e6SR0mf4UXiiA5oxkqidfep50GqXJeH2pZTrl6JgZTcmaxLo7B76TrWXp1F8kJAR2xQLsfQT+fSNB7Wt+wtBd2tEHA2jME3YYI3iHgI3ZHbbxWM6w5O5vlG+JgV5YR5tGBCI4ySFWBAshGD8boiVos5kEfb5Eyo3HI8D2Vb2wyOMMkSHOOehhQVNyJl5iXyXmMzBrumCq6feHkka2QNie0fsoJmw11uef9qOi1nxzEd++YcmiFGMse4S7KrwDx4XL8KyixBbJ4clWmAWC12tAXJ7EW11ZQXIrUDfZNfYJduFbfHNEi1Oqm2i8Vtr1xU3ZZ9p6OeVXkEIkh8eR18cnS84qQxb1jSvWXDqnguWy96nvoDqmD1eYMuB7a8OPoMRyK2ZWO5aTziRJf653jzUCTG2lZ8K815oFeFK5MaLQa6VDw9P5NGl7NUATSjfk2zGKjritXxepOeGuGpf7O16uQ25HD47GlRY652nav4H2FDp7yzeVPq2qLYb7Mokm1oBMcBRHBbq9iYrQR5tlhxEE+B56HQ/fL7DPc5+wcdk6b++gwAAA==
+var rule = {
+  title: 'HBOTV[优]',
+  host: 'https://www.hbotv2.com/',
+  url: '/fyclass/index_fypage.html[/fyclass/index.html]',
+  searchUrl: '/vodsearch/**-fypage/',
+  searchable: 2,
+  quickSearch: 0,
+  filterable: 0,
+  headers: {
+    'User-Agent': 'PC_UA',
+  },
+  class_parse: '.stui-header__menu li;a&&Text;a&&href;.*/(.*?)/',
+  play_parse: true,
+  lazy: "js:\n  let html = request(input);\n  let hconf = html.match(/r player_.*?=(.*?)</)[1];\n  let json = JSON5.parse(hconf);\n  let url = json.url;\n  if (json.encrypt == '1') {\n    url = unescape(url);\n  } else if (json.encrypt == '2') {\n    url = unescape(base64Decode(url));\n  }\n  if (/\\.(m3u8|mp4|m4a|mp3)/.test(url)) {\n    input = {\n      parse: 0,\n      jx: 0,\n      url: url,\n    };\n  } else {\n    input;\n  }",
+  limit: 6,
+  double: true,
+  推荐: 'ul.stui-vodlist.clearfix;li;a&&title;.lazyload&&data-original;.pic-text&&Text;a&&href',
+  一级: 'ul.stui-vodlist.clearfix li;a&&title;.lazyload&&data-original;.pic-text&&Text;a&&href',
+  二级: {
+    title: '.stui-content__detail .title&&Text;.stui-content__detail&&p:eq(3)&&a&&Text',
+    img: '.stui-content__thumb .lazyload&&data-original',
+    desc: '.pic-text&&Text;.stui-content__detail&&p:eq(3)&&a:eq(2)&&Text;.stui-content__detail&&p:eq(3)&&a:eq(1)&&Text;.stui-content__detail p--span&&Text;.stui-content__detail p:eq(1)--span&&Text',
+    content: '.detail-sketch&&Text',
+    tabs: '.stui-pannel__head h3',
+    lists: '.stui-content__playlist:eq(#id) li',
+  },
+  搜索: $js.toString(() => {
+        var d = [];
+        var body = 'keyboard=' + KEY + '&show=title&tempid=1&tbname=news&mid=1&dopost=search'; //log(body)
+        var headers = `{
+            'Host': 'www.hbotv1.com',
+            'content-length': '79',
+            'cache-control': 'max-age=0',
+            'upgrade-insecure-requests': '1',
+            'origin': 'https://www.hbotv1.com',
+            'content-type': 'application/x-www-form-urlencoded',
+            'user-agent': 'Mozilla/5.0 (Linux; Android 12; 22021211RC Build/SKQ1.211006.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/99.0.4844.88 Mobile Safari/537.36',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'x-requested-with': 'com.example.hikerview',
+            'sec-fetch-site': 'same-origin',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-user': '?1',
+            'sec-fetch-dest': 'document',
+            'referer': 'https://www.hbotv2.com/vodsearch/203070-0/',
+            'accept-language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7'
+        }`;
+        var html = fetch('https://www.hbotv2.com/e/search/index.php', {
+            headers: headers,
+            body: body,
+            method: 'POST'
+        }); //log(html)
+        
+        var list = pdfa(html, '.stui-vodlist__media&&li');
+        for (var j in list) {
+            d.push({
+                title: pdfh(list[j], '.lazyload&&title'),
+                desc: pdfh(list[j], '.pic-text&&Text'),
+                img: pd(list[j], '.lazyload&&data-original') + '@Referer=',
+                url: 'https://www.hbotv1.com'+pdfh(list[j], '.lazyload&&href') + '#immersiveTheme#'
+            });
+        }
+        setResult(d);
+    }),
+}
