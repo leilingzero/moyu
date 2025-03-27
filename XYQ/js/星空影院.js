d12628d5538fd5101f243df7a6dd7c3379411d44
leilingzero <66689331+leilingzero@users.noreply.github.com>
2025-03-27 11:39:47 +0800

    Update 星空影院.js

--- XYQ/js/星空影院.js
+++ XYQ/js/星空影院.js
@@ -1,1 +1,9 @@
-H4sIAIioBmcC/32PPQrCQBCFe0+xlf/J1ireQjtBxmTjBjdu2J3EBPEKClZaiGilpa0gnsaIt3ANAa2cZmbex3vMxKCIigQjXTIvEVPZ6ZDt7m1SCZJQyUozF9FHwYyWbfbP8/Vxu7y2xwJxqdEQjhjqNqXJJE1btiODAkdKGEpj6WouZ9RLHQFaW0V5aQhjZgabYyAKi2agHN7/GvOd1uuW9deXJw9DUPpzqj2FeATK8pEFmgi/A+VyjyX46Vwxr0Org4HbqP0mIIyGLHFE5ObPLteP68qgxRt4sUvsJQEAAA==
+var rule = {
+    模板: 'mxpro',
+    title: '星空影院',
+    host: 'https://xkyy10.com',
+    url: '/vodshow/fyclass--------fypage---.html',
+    searchUrl: '/vodsearch/**----------fypage---.html',
+    class_parse: '.navbar-items li;a&&Text;a&&href;/(\\d+).html',
+    tab_exclude: '排序',
+}
